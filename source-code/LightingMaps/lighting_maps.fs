c32050a9b3523af17e1393264ea6bda570977ff1
longyue <184579801@qq.com>
2022-05-25 17:01:30 +0800

    DFDSF

--- source-code/LightingMaps/lighting_maps.fs
+++ source-code/LightingMaps/lighting_maps.fs
@@ -1,14 +1,16 @@
 #version 330 core
 out vec4 FragColor;
 
-struct Material {
+struct Material 
+{
     vec3 ambient;
     sampler2D diffuse;
     sampler2D specular;
     float shininess;
 }; 
 
-struct Light {
+struct Light 
+{
     vec3 position;
     vec3 ambient;
     vec3 diffuse;
@@ -32,15 +34,12 @@ void main()
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-   // vec3 diffuse = light.diffuse * (diff * material.diffuse);
-    
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
 
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    //vec3 specular = light.specular * (spec * material.specular);  
     vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
     vec3 result = ambient + diffuse + specular;
