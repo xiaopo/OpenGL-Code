55a0c29f434287162947e8ff94aa5d1c4167ee93
跃 <184579801@qq.com>
2022-06-02 00:06:58 +0800

    stencil_test
    stencil_test

--- source-code/depth_testing/depth_testing.fs
+++ source-code/depth_testing/depth_testing.fs
@@ -23,5 +23,5 @@ void main()
     //FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
 
     float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
-    FragColor = vec4(vec3(depth), 1.0);
+    FragColor = vec4(depth,0,0, 1.0);
 }
